0eede13a252267b4f2dcef2690ab83545001e225
chrisdugne <chris.dugne@uralys.com>
2019-09-20 18:41:47 +0200

    added os parsing

--- src/pages/router.js
+++ src/pages/router.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import UAParser from 'ua-parser-js';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -10,71 +11,53 @@ const PLAYSTORE = {
     'https://play.google.com/store/apps/details?id=com.uralys.kodo.monster'
 };
 
-// const APPSTORE = {
-//   exalt: '',
-//   kodo: 'https://apps.apple.com/us/app/kodo-find-the-color/id684227637',
-//   'kodo-monster': 'https://apps.apple.com/us/app/id1462688847'
-// };
+const APPSTORE = {
+  exalt: 'https://apps.apple.com/us/app/testflight/id899247664',
+  kodo: 'https://apps.apple.com/us/app/kodo-find-the-color/id684227637',
+  'kodo-monster': 'https://apps.apple.com/us/app/id1462688847'
+};
 
-function Redirect({ app, params }) {
-  function openStore(e) {
-    const splitters = document.URL.split('/');
-    const game = splitters[splitters.length - 3];
-
-    if (PLAYSTORE[game]) {
-      window.location.href = PLAYSTORE[game];
-    } else {
-      window.location.href = '/';
-    }
-
-    // const userAgent = new UAParser(event.headers['User-Agent']);
-    // const { os: platform } = userAgent.getResult();
-    // switch (platform.name) {
-    //   case 'Android':
-    //     return {
-    //       statusCode: 301,
-    //       headers: {
-    //         Location: storeLink.android(APP_ID)
-    //       }
-    //     };
-    //   case 'iOS':
-    //     return {
-    //       statusCode: 301,
-    //       headers: {
-    //         Location: storeLink.ios(APP_STORE_ID)
-    //       }
-    //     };
-    //   default:
-    //     return {
-    //       statusCode: 409,
-    //       body: 'This link should be opened in Android or iOS.'
-    //     };
-    // }
-  }
-
-  function copyToClipboard() {
+const Redirect = ({ app, params }) => {
+  const copyToClipboard = () => {
     const copyText = document.getElementById('textarea');
     copyText.value = document.URL;
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand('copy');
-  }
+  };
 
-  function validate(e) {
+  const validate = redirection => event => {
     copyToClipboard();
-    e.preventDefault();
-    openStore();
+    event.preventDefault();
+    window.location.href = redirection;
+  };
+
+  let info = `You'll be redirected to the stores to download "${app}".`;
+
+  const redirection = '/';
+  const parser = new UAParser();
+  const os = parser.getOS().name;
+  console.log({ os });
+
+  switch (os) {
+    case 'Android':
+      redirection = PLAYSTORE[app];
+      break;
+    case 'iOS':
+      redirection = APPSTORE[app];
+      break;
+    default:
+      info = 'This link should be opened in Android or iOS.';
   }
 
   return (
-    <form onSubmit={validate}>
+    <form onSubmit={validate(redirection)}>
       <textarea style={{ opacity: 0 }} id="textarea" />
-      {/* <textarea style={{ display: 'none' }} id="textarea" /> */}
-      <p>{`You'll be redirected to the stores to download "${app}".`}</p>
+      <p>{info}</p>
       <button>Continue</button>
     </form>
   );
-}
+};
 
 const Router = context => {
   const slugs = context.location.pathname.split('/');
